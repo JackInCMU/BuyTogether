3ea69a45322b7a719066cf4078c2ee495638523d
zhiqi <zhiqili@andrew.cmu.edu>
2022-03-19 23:46:50 -0400

    shopmain first complete version

--- screens/ShopMainScreen.tsx
+++ screens/ShopMainScreen.tsx
@@ -8,6 +8,8 @@ import { Feather } from '@expo/vector-icons';
 import { SearchBar } from 'react-native-elements/dist/searchbar/SearchBar';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { color } from 'react-native-elements/dist/helpers';
+import Color from '../constants/Color';
   
   
 
@@ -37,47 +39,148 @@ export default function ShopMainScreen({navigation}: RootTabScreenProps<'TabOne'
             <View style={{height:35, marginTop:15, borderBottomWidth:3, borderBottomColor:'#085B26', backgroundColor:'#ffffff'}}>
                 <ScrollView horizontal={true} contentContainerStyle={{ flexGrow: 1 }}>
                     <View style={{backgroundColor:'#ffffff'}}>
-                        <Text style={{fontWeight:'700',color:'#085B26',marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>Vegetables</Text>
+                        <Text style={styles.category}>Vegetables</Text>
                         <View style = {styles.highlight}></View>
                     </View>
-                    <View style={{marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>
+                    <View style={styles.category}>
                         <Text style={{fontWeight:'700',color:'#085B26'}}>Fruits</Text>
                     </View>
-                    <View style={{marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>
+                    <View style={styles.category}>
                         <Text style={{fontWeight:'700',color:'#085B26'}}>Beverage</Text>
                     </View>
-                    <View style={{marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>
+                    <View style={styles.category}>
                         <Text style={{fontWeight:'700',color:'#085B26'}}>Snacks</Text>
                     </View>
-                    <View style={{marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>
+                    <View style={styles.category}>
                         <Text style={{fontWeight:'700',color:'#085B26'}}>Fruits</Text>
                     </View>
-                    <View style={{marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>
+                    <View style={styles.category}>
                         <Text style={{fontWeight:'700',color:'#085B26'}}>Diary</Text>
                     </View>
-                    <View style={{marginLeft:25, backgroundColor:'#ffffff', marginTop:5}}>
+                    <View style={styles.category}>
                         <Text style={{fontWeight:'700',color:'#085B26'}}>Fruits</Text>
                     </View>
                 </ScrollView>
             </View>
-            <View style={{width:400,}}>
-                <ScrollView contentContainerStyle={{ flexGrow: 1}}>
-                    <View style={{flexDirection:'row'}}>
-                        <View style={styles.item}>
-                        <Image style = {styles.item_pic} source={require("../assets/images/chilli.jpeg")}/>
+            <View style={{flexDirection:'row',flex:1}}>
+                <ScrollView contentContainerStyle={{flexGrow: 1}}>
+                    <View style={{flexDirection:'row',justifyContent:'space-evenly', backgroundColor:'transparent'}}>
+                        <View style={styles.shadow}>
+                            <View style={styles.item}>
+                                <Image style = {styles.item_pic} source={require("../assets/images/carrot.jpeg")}/>
+                                <View style = {styles.item_tag_box}>
+                                    <View style={styles.name_price_tag}>
+                                        <Text style={styles.item_name}>Carrots</Text>
+                                        <View style={{flexDirection:'row'}}>
+                                            <Text style={styles.price}>$0.99</Text><Text style={styles.unit}> / lb</Text>
+                                        </View>
+                                    </View>
+                                    <View style = {styles.item_plus}>
+                                        <Feather name="plus-square" size={35} color ='#4CAD73'/>
+                                    </View>
+                                </View>
+                            </View> 
+                        </View>   
 
-                        </View>
-                        <View style={styles.item}>
+                        <View style={styles.shadow}>
+                            <View style={styles.item}>
+                                <Image style = {styles.item_pic} source={require("../assets/images/chilli.jpeg")}/>
+                                <View style = {styles.item_tag_box}>
+                                    <View style={styles.name_price_tag}>
+                                        <Text style={styles.item_name}>Chilli</Text>
+                                        <View style={{flexDirection:'row'}}>
+                                            <Text style={styles.price}>$0.99</Text><Text style={styles.unit}> / lb</Text>
+                                        </View>
+                                    </View>
+                                    <View style = {styles.item_plus}>
+                                        <Feather name="minus-square" size={20} color="#4CAD73"/>
+                                        <Text style={{backgroundColor:'#d6ecdf',width:20, textAlign:'center'}}>4</Text>
+                                        <Feather name="plus-square" size={20} color ='#4CAD73'/>  
+                                    </View>
+                                </View>
+                            </View>
                         </View>
                     </View>
 
-                    <View style={{flexDirection:'row'}}>
-                        <View style={styles.item}>
+                    <View style={{flexDirection:'row',justifyContent:'space-evenly', backgroundColor:'transparent'}}>
+                        <View style={styles.shadow}>
+                            <View style={styles.item}>
+                                <Image style = {styles.item_pic} source={require("../assets/images/onion.jpeg")}/>
+                                <View style = {styles.item_tag_box}>
+                                    <View style={styles.name_price_tag}>
+                                        <Text style={styles.item_name}>Onion</Text>
+                                        <View style={{flexDirection:'row'}}>
+                                            <Text style={styles.price}>$0.99</Text><Text style={styles.unit}> / lb</Text>
+                                        </View>
+                                    </View>
+                                    <View style = {styles.item_plus}>
+                                        <Feather name="plus-square" size={35} color ='#4CAD73'/>
+                                    </View>
+                                </View>
+                            </View> 
+                        </View>   
+
+                        <View style={styles.shadow}>
+                            <View style={styles.item}>
+                                <Image style = {styles.item_pic} source={require("../assets/images/cucumber.jpeg")}/>
+                                <View style = {styles.item_tag_box}>
+                                    <View style={styles.name_price_tag}>
+                                        <Text style={styles.item_name}>Cucumber</Text>
+                                        <View style={{flexDirection:'row'}}>
+                                            <Text style={styles.price}>$0.99</Text><Text style={styles.unit}> / lb</Text>
+                                        </View>
+                                    </View>
+                                    <View style = {styles.item_plus}>
+                                        <Feather name="plus-square" size={35} color ='#4CAD73'/>
+                                    </View>
+                                </View>
+                            </View>
                         </View>
-                        <View style={styles.item}>
+                    </View>
+
+                    
+                    <View style={{flexDirection:'row',justifyContent:'space-evenly', backgroundColor:'transparent'}}>
+                        <View style={styles.shadow}>
+                            <View style={styles.item}>
+                                <Image style = {styles.item_pic} source={require("../assets/images/carrot.jpeg")}/>
+                                <View style = {styles.item_tag_box}>
+                                    <View style={styles.name_price_tag}>
+                                        <Text style={styles.item_name}>Carrots</Text>
+                                        <View style={{flexDirection:'row'}}>
+                                            <Text style={styles.price}>$0.99</Text><Text style={styles.unit}> / lb</Text>
+                                        </View>
+                                    </View>
+                                    <View style = {styles.item_plus}>
+                                        <Feather name="plus-square" size={35} color ='#4CAD73'/>
+                                    </View>
+                                </View>
+                            </View> 
+                        </View>   
+
+                        <View style={styles.shadow}>
+                            <View style={styles.item}>
+                                <Image style = {styles.item_pic} source={require("../assets/images/chilli.jpeg")}/>
+                                <View style = {styles.item_tag_box}>
+                                    <View style={styles.name_price_tag}>
+                                        <Text style={styles.item_name}>Chilli</Text>
+                                        <View style={{flexDirection:'row'}}>
+                                            <Text style={styles.price}>$0.99</Text><Text style={styles.unit}> / lb</Text>
+                                        </View>
+                                    </View>
+                                    <View style = {styles.item_plus}>
+                                        <Feather name="minus-square" size={20} color="#4CAD73"/>
+                                        <Text style={{backgroundColor:'#d6ecdf',width:20, textAlign:'center'}}>4</Text>
+                                        <Feather name="plus-square" size={20} color ='#4CAD73'/>  
+                                    </View>
+                                </View>
+                            </View>
                         </View>
+                    </View>                    
+                    <View style={styles.test}>
+                        <View style ={styles.test_inner}></View>
                     </View>
                 </ScrollView>
+
             </View>
         </View>
     );
@@ -124,6 +227,13 @@ const styles = StyleSheet.create({
         fontSize:12,
         paddingHorizontal:3,
     },
+    category:{
+        fontWeight:'700',
+        color:'#085B26',
+        marginLeft:25, 
+        backgroundColor:'#ffffff', 
+        marginTop:5,
+    },
     highlight:{
         width: 66,
         height:5,
@@ -134,21 +244,69 @@ const styles = StyleSheet.create({
         backgroundColor:'#085B26'
     },
     item:{
-        flex:1,
-        alignItems:'flex-start',
+        flexDirection:'column',
         height:185,
-        width:155,
-        borderWidth:2,
-        borderColor:'black',
+        width:150,
         borderRadius:10,
-        backgroundColor:'#ffffff',
-        marginTop:25,
-        marginLeft:30,
-        overflow:'hidden'
+        marginTop:20,
+        overflow:'hidden',
+        shadowColor: '#000000',
+        elevation:10,
     },
     item_pic:{
-        aspectRatio: 3/3,
-        height:50,
+        aspectRatio: 3/2,
+        height:120,
+    },
+    item_tag_box:{
+        flexDirection:'row',
+        backgroundColor:'#ffffff',
+        height:65,
+        alignItems:'center',
+        justifyContent:'space-around'
+    },
+    item_plus:{
+        flexDirection:'row',
+        width:60,
+        height:40,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    name_price_tag:{
+        flexDirection:'column',
+        width:70,
+        alignItems:'flex-start'
+    },
+    item_name:{
+        marginBottom:5,
+        fontWeight:'600',
+        fontSize:14,
+    },
+    price:{
+        fontWeight:'700',
+        fontSize:13,
+        color:'#4CAD73'
+    },
+    unit:{
+        color:'#bdbdbd',
+        fontSize:12,
+    },
+    shadow:{
+        shadowOffset: { width: 3, height: 5},
+        shadowColor: '#bdbdbd',
+        shadowOpacity: 10,
+        elevation: 2,
+        // background color must be set
+        backgroundColor : "#0000" 
+    },
+    test:{
+        backgroundColor:'transparent',
+        flexDirection:'column',
+        alignItems:'center'
+    },
+    test_inner:{
+        width:100,
+        height:10,
+        backgroundColor:'orange'
     }
 
 });
